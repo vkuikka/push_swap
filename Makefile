920ae8a30190d5e0dc475964a3a59d5b4dbd01d0
Ville Kuikka <vkuikka@student.hive.fi>
2020-08-06 15:36:36 +0300

    fixed some errors in checker and cleaned up the code

--- Makefile
+++ Makefile
@@ -6,13 +6,13 @@
 #    By: vkuikka <vkuikka@student.hive.fi>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/12/08 13:46:11 by vkuikka           #+#    #+#              #
-#    Updated: 2020/07/29 17:42:14 by vkuikka          ###   ########.fr        #
+#    Updated: 2020/08/06 15:18:16 by vkuikka          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = push_swap
-OPERATIONS = distance.c swap_operations.c rotate_operations.c
-EXTRAS = ft_simplify.c stack_iteration.c
+OPERATIONS = distance.c swap_operations.c rotate_operations.c stack_iteration.c
+EXTRAS = ft_simplify.c
 LIB = libft/libft.a
 INCLUDE = libft/includes
 .PHONY: clean fclean re all
@@ -22,7 +22,7 @@ all: $(NAME)
 $(NAME):
 	#make -C libft
 	gcc -o $(NAME) push_swap.c $(OPERATIONS) $(LIB) $(EXTRAS) -I $(INCLUDE)
-	#gcc -o checker checker.c $(OPERATIONS) $(LIB) -I $(INCLUDE)
+	gcc -o checker checker.c $(OPERATIONS) $(LIB) -I $(INCLUDE)
 	#gcc -o $(NAME) -Wall -Wextra -Werror $(OPERATIONS) $(LIB) $(EXTRAS) -I $(INCLUDE)
 
 clean:
--- checker.c
+++ checker.c
@@ -3,119 +3,121 @@
 /*                                                        :::      ::::::::   */
 /*   checker.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vkuikka <vkuikka@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: vkuikka <vkuikka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/29 17:28:20 by vkuikka           #+#    #+#             */
-/*   Updated: 2020/07/29 17:28:21 by vkuikka          ###   ########.fr       */
+/*   Updated: 2020/08/06 15:32:52 by vkuikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "get_next_line.h"
 
-static int ft_swap_operations(int **stacks, int *lengths, char *instruction)
+static int	ft_swap_operations(int **stacks, int *lengths, char *instruction)
 {
+	int		ret;
+
+	ret = -1;
 	if (!ft_strcmp(instruction, "sa"))
-		ft_swap_ps(stacks[0], lengths[0]);
+		ret = ft_swap_ps((unsigned *)stacks[0], lengths[0]);
 	else if (!ft_strcmp(instruction, "sb"))
-		ft_swap_ps(stacks[1], lengths[1]);
+		ret = ft_swap_ps((unsigned *)stacks[1], lengths[1]);
 	else if (!ft_strcmp(instruction, "ss"))
-		ft_ss(stacks[0], stacks[1], lengths[0], lengths[1]);
+		ret = ft_ss((unsigned *)stacks[0], (unsigned *)stacks[1],
+				lengths[0], lengths[1]);
 	else if (!ft_strcmp(instruction, "pb"))
 	{
-		ft_push(stacks[0], stacks[1], lengths[0], lengths[1]);
+		ret = ft_push((unsigned *)stacks[0], (unsigned *)stacks[1],
+				lengths[0], lengths[1]);
 		lengths[0]--;
 		lengths[1]++;
 	}
 	else if (!ft_strcmp(instruction, "pa"))
 	{
-		ft_push(stacks[1], stacks[0], lengths[1], lengths[0]);
+		ret = ft_push((unsigned *)stacks[1], (unsigned *)stacks[0],
+				lengths[1], lengths[0]);
 		lengths[1]--;
 		lengths[0]++;
 	}
-	else
-		return (0);
-	return (1);
+	return (ret);
 }
 
-static int ft_rotate_operations(int **stacks, int *lengths, char *instruction)
+static int	ft_rotate_operations(int **stacks, int *lengths, char *instruction)
 {
+	int		ret;
+
+	ret = -1;
 	if (!ft_strcmp(instruction, "ra"))
-		ft_rotate(stacks[0], lengths[0]);
+		ret = ft_rotate((unsigned *)stacks[0], lengths[0]);
 	else if (!ft_strcmp(instruction, "rb"))
-		ft_rotate(stacks[1], lengths[1]);
+		ret = ft_rotate((unsigned *)stacks[1], lengths[1]);
 	else if (!ft_strcmp(instruction, "rr"))
-		ft_rr(stacks[0], stacks[1], lengths[0], lengths[1]);
+		ret = ft_rr((unsigned *)stacks[0], (unsigned *)stacks[1],
+				lengths[0], lengths[1]);
 	else if (!ft_strcmp(instruction, "rra"))
-		ft_rrotate(stacks[0], lengths[0]);
+		ret = ft_rrotate((unsigned *)stacks[0], lengths[0]);
 	else if (!ft_strcmp(instruction, "rrb"))
-		ft_rrotate(stacks[1], lengths[1]);
+		ret = ft_rrotate((unsigned *)stacks[1], lengths[1]);
 	else if (!ft_strcmp(instruction, "rrr"))
-		ft_rrr(stacks[0], stacks[1], lengths[0], lengths[1]);
-	else
-		return (0);
-	return (1);
-}
-
-static int	ft_EOI_check(int **stacks, int *lengths)
-{
-	int		i;
-
-	i = 0;
-	if (lengths[1])
-		return (0);
-	if (lengths[0] == 1)
-		return (1);
-	while (++i < lengths[0])
-		if (stacks[0][i - 1] > stacks[0][i])
-			return (0);
-	return (1);
+		ret = ft_rrr((unsigned *)stacks[0], (unsigned *)stacks[1],
+				lengths[0], lengths[1]);
+	return (ret);
 }
 
 static int	ft_evaluate(int **stacks, int *lengths)
 {
 	char	*instruction;
-	char	*last_instr;
-	int		solved;
-	int		i;
+	int		tmp;
+	int		tmp2;
 
-	solved = 0;
-	last_instr = NULL;
 	ft_putnbr_arr(&stacks[0], 1, lengths[0]);
 	ft_putnbr_arr(&stacks[1], 1, lengths[1]);
-	while (!solved)
+	while (0 < (tmp = get_next_line(0, &instruction)))
 	{
-		i = -1;
-		if (!get_next_line(0, &instruction))
-			return (ft_EOI_check(stacks, lengths));	//stop if ctrl+D is pressed (end of input)
-		if (!ft_rotate_operations(stacks, lengths, instruction) &&
-			!ft_swap_operations(stacks, lengths, instruction))
-		{
-			ft_putstr("Error\n");
-			exit (0);		//stop if input doesnt match any real instruction
-		}
+		if (0 >= (tmp = ft_rotate_operations(stacks, lengths, instruction)) &&
+			0 >= (tmp2 = ft_swap_operations(stacks, lengths, instruction)))
+			return (tmp > tmp2 ? tmp : tmp2);
 		ft_putnbr_arr(&stacks[0], 1, lengths[0]);
 		ft_putnbr_arr(&stacks[1], 1, lengths[1]);
-		last_instr = instruction;
+	}
+	if (tmp < 0)
+		return (-1);
+	if (lengths[1])
+		return (0);
+	while (tmp < lengths[0] - 1)
+	{
+		if (stacks[0][tmp] > stacks[0][tmp + 1])
+			return (0);
+		tmp++;
 	}
 	return (1);
 }
 
-static int	ft_check(char *str)
+static int	ft_check_input(char **nums, int **stacks, int len)
 {
-	if (ft_strcmp(str, ft_itoa(ft_atoi(str))))
+	int		i;
+	int		j;
+
+	i = 0;
+	while (i < len)
 	{
-		ft_putstr("Error\n");
-		return (0);
-	}
-	while (*str)
-	{
-		if ((*str > '9' || *str < '0') && *str != '-')
-		{
-			ft_putstr("Error\n");
+		if (ft_strcmp(nums[i], ft_itoa(ft_atoi(nums[i]))))
 			return (0);
+		j = 0;
+		while (nums[i][j])
+		{
+			if ((nums[i][j] > '9' || nums[i][j] < '0') && nums[i][j] != '-')
+				return (0);
+			j++;
 		}
-		str++;
+		j = i;
+		stacks[0][i] = ft_atoi(nums[i]);
+		while (j--)
+		{
+			if (stacks[0][j] == stacks[0][i])
+				return (0);
+		}
+		i++;
 	}
 	return (1);
 }
@@ -124,24 +126,24 @@ int			main(int argc, char **argv)
 {
 	int		**stacks;
 	int		lengths[2];
-	int		i;
+	int		tmp;
 
 	if (argc == 1 ||
 		(!(stacks = (int **)malloc(sizeof(int*) * 2))) ||
 		(!(stacks[0] = (int *)malloc(sizeof(int) * argc - 1))) ||
 		(!(stacks[1] = (int *)malloc(sizeof(int) * argc - 1))))
 		return (1);
-	i = 0;
-	while (++i < argc)
+	if (!ft_check_input(argv + 1, stacks, argc - 1))
 	{
-		if (!ft_check(argv[i]))
-			return (0);
-		stacks[0][i - 1] = ft_atoi(argv[i]);
+		ft_putstr("Error\n");
+		return (1);
 	}
 	lengths[0] = argc - 1;
 	lengths[1] = 0;
-	if (ft_evaluate(stacks, lengths))
+	if (0 < (tmp = ft_evaluate(stacks, lengths)))
 		ft_putstr("OK\n");
+	else if (tmp < 0)
+		ft_putstr("Error\n");
 	else
 		ft_putstr("KO\n");
 	return (0);
